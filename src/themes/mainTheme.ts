02b7c7e503da30bfc9d576099dc480f0b22a8bd5
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-08 18:13:05 +0400

    add theme

---
+++ src/themes/mainTheme.ts
@@ -0,0 +1,20 @@
+const sizes = {
+  md: "768px",
+};
+
+const theme = {
+  sizes,
+
+  colors: {
+    primary: "#315efb",
+    white: "#ffffff",
+    grey: "#e3eaf0",
+    greyLight: "#f4f6f9",
+    greyText: "#aeb4bb",
+    yellow: "#fce463",
+    dark: "#1d2734",
+    black: "#000000",
+  },
+};
+
+export default theme;
