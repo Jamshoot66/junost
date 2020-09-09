823191e849d5d6ef3ef4de23050b137e923981c9
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-09 22:52:29 +0400

    fix type definition

--- src/package/Grade/Grade.tsx
+++ src/package/Grade/Grade.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 interface Props {
   label: string;
-  skills: [
-    {
-      label: string;
-      description: string;
-      links: string[];
-    }
-  ];
+  skills: {
+    label: string;
+    description: string;
+    links: string[];
+  }[];
 }
 
 function Grade(props: Props): React.ReactElement {
