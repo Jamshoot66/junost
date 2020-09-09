3c070f85790adde9d22688bbbdd5778c170212f7
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-09 22:48:59 +0400

    add grades container to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import PageContainer from "components/PageContainer";
 import PageHeader from "components/PageHeader";
+import GradesContainer from "components/GradesContainer";
 
 class Index extends React.Component {
-  componentDidCatch(error: any, errorInfo: any): void {
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
     // eslint-disable-next-line no-console
     console.error("errorInfo", error);
     // eslint-disable-next-line no-console
     console.error("errorInfo", errorInfo);
   }
 
-  render() {
+  render(): React.ReactElement {
     return (
       <PageContainer>
         <PageHeader />
+        <GradesContainer />
       </PageContainer>
     );
   }
