cbd69019ab9cb48552b1ed900cef62c0f73ab35d
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-09 21:37:04 +0400

    add generic page container

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "themes/mainTheme";
+import { Container } from "./PageContainer.ui";
 
 interface Props {
   children: React.ReactElement;
@@ -8,7 +9,11 @@ interface Props {
 
 function PageContainer(props: Props) {
   const { children } = props;
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>{children}</Container>
+    </ThemeProvider>
+  );
 }
 
 export default PageContainer;
