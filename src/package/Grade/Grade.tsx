0f329b40ff1a056e75b7f2245574a5c6632954cf
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-09 22:43:09 +0400

    add grade template

---
+++ src/package/Grade/Grade.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface Props {
+  label: string;
+  skills: [
+    {
+      label: string;
+      description: string;
+      links: string[];
+    }
+  ];
+}
+
+function Grade(props: Props): React.ReactElement {
+  const { label, skills } = props;
+
+  return (
+    <section>
+      <header>{label}</header>
+      <div>
+        {skills?.map(skill => (
+          <div>{skill.label}</div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Grade;
