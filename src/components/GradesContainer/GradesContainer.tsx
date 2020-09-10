fa3cf49db630e5ea1661a08c3ce5c656b80f19ca
Сергей Горшков <sergey.gorshkov@anmedio.ru>
2020-09-10 11:07:14 +0400

    ref container

--- src/components/GradesContainer/GradesContainer.tsx
+++ src/components/GradesContainer/GradesContainer.tsx
@@ -1,100 +1,55 @@
 import React from "react";
 import Grade from "package/Grade";
+import { JUNOST_GIT_ISSUES_URL } from "utils/constants";
 import {
   Container,
   Title,
   GradeContainer,
   AddMoreButton,
   SingleGradeContainer,
+  PlusIcon,
+  VerticalPlaceholder,
 } from "./GradesContainer.ui";
+import { mockedGradeSections } from "./mockData";
 
-class GradesContainer extends React.PureComponent {
-  gradeSections = [
-    {
-      title: "Frontend",
-      grades: [
-        {
-          label: "Junior",
-          skills: [
-            {
-              label: "Javascript",
-              description: "Javascript description",
-              links: ["google.com", "ya.ru"],
-            },
-            {
-              label: "Typescript",
-              description: "Typescript description",
-              links: ["google.com", "ya.ru"],
-            },
-          ],
-        },
-        {
-          label: "Middle",
-          skills: [
-            {
-              label: "Javascript",
-              description: "Javascript description",
-              links: ["google.com", "ya.ru"],
-            },
-            {
-              label: "Typescript",
-              description: "Typescript description",
-              links: ["google.com", "ya.ru"],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      title: "Frontend 2",
-      grades: [
-        {
-          label: "Junior",
-          skills: [
-            {
-              label: "Javascript",
-              description: "Javascript description",
-              links: ["google.com", "ya.ru"],
-            },
-            {
-              label: "Typescript",
-              description: "Typescript description",
-              links: ["google.com", "ya.ru"],
-            },
-          ],
-        },
-        {
-          label: "Middle",
-          skills: [
-            {
-              label: "Javascript",
-              description: "Javascript description",
-              links: ["google.com", "ya.ru"],
-            },
-            {
-              label: "Typescript",
-              description: "Typescript description",
-              links: ["google.com", "ya.ru"],
-            },
-          ],
-        },
-      ],
-    },
-  ];
+interface State {
+  isCollapsed: boolean;
+}
+
+class GradesContainer extends React.PureComponent<never, State> {
+  gradeSections = mockedGradeSections;
+
+  state: State = {
+    isCollapsed: true,
+  };
 
   render(): React.ReactElement {
+    const { isCollapsed } = this.state;
     return (
       <Container>
         {this.gradeSections?.map(section => (
           <GradeContainer>
-            <Title>{section.title}</Title>
+            <Title>
+              <PlusIcon> {isCollapsed ? "+" : "-"}</PlusIcon>
+              {section.title}
+            </Title>
 
             {section.grades?.map(grade => (
               <SingleGradeContainer>
                 <Grade
                   label={grade.label}
                   skills={grade.skills}
-                  footer={<AddMoreButton>Добавить + </AddMoreButton>}
+                  footer={
+                    <React.Fragment>
+                      <VerticalPlaceholder />
+                      <AddMoreButton
+                        href={JUNOST_GIT_ISSUES_URL}
+                        target="_blank"
+                      >
+                        Добавить +
+                      </AddMoreButton>
+                    </React.Fragment>
+                  }
                 />
               </SingleGradeContainer>
             ))}
