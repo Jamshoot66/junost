6554bc7f6c1b3e5c5e0e1892cdd505f9e13b16bf
Сергей Горшков <sergey.gorshkov@anmedio.ru>
2020-09-10 11:06:45 +0400

    ref mocked data

---
+++ src/components/GradesContainer/mockData.ts
@@ -0,0 +1,73 @@
+// eslint-disable-next-line import/prefer-default-export
+export const mockedGradeSections = [
+  {
+    title: "Frontend",
+    grades: [
+      {
+        label: "Junior",
+        skills: [
+          {
+            label: "Javascript",
+            description: "Javascript description",
+            links: ["google.com", "ya.ru"],
+          },
+          {
+            label: "Typescript",
+            description: "Typescript description",
+            links: ["google.com", "ya.ru"],
+          },
+        ],
+      },
+      {
+        label: "Middle",
+        skills: [
+          {
+            label: "Javascript",
+            description: "Javascript description",
+            links: ["google.com", "ya.ru"],
+          },
+          {
+            label: "Typescript",
+            description: "Typescript description",
+            links: ["google.com", "ya.ru"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Frontend 2",
+    grades: [
+      {
+        label: "Junior",
+        skills: [
+          {
+            label: "Javascript",
+            description: "Javascript description",
+            links: ["google.com", "ya.ru"],
+          },
+          {
+            label: "Typescript",
+            description: "Typescript description",
+            links: ["google.com", "ya.ru"],
+          },
+        ],
+      },
+      {
+        label: "Middle",
+        skills: [
+          {
+            label: "Javascript",
+            description: "Javascript description",
+            links: ["google.com", "ya.ru"],
+          },
+          {
+            label: "Typescript",
+            description: "Typescript description",
+            links: ["google.com", "ya.ru"],
+          },
+        ],
+      },
+    ],
+  },
+];
