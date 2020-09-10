e8bf6b81d867b647a50529610a20ac8024e5a977
Сергей Горшков <sergey.gorshkov@anmedio.ru>
2020-09-10 12:03:51 +0400

    add Grade hoc

---
+++ src/package/Grade/Grade.hoc.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Grade from "./Grade";
+
+export default React.memo(Grade);
--- src/package/Grade/index.ts
+++ src/package/Grade/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./Grade";
+export { default } from "./Grade.hoc";
