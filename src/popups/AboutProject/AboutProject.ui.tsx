c58d4ca9ae8d6d94517037e44cd082bb9878af36
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-15 13:57:43 +0400

    add about page

---
+++ src/popups/AboutProject/AboutProject.ui.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+// eslint-disable-next-line import/prefer-default-export
+export const SubHeader = styled.h4`
+  font-weight: bold;
+  font-size: 1.6rem;
+  margin-bottom: 19px;
+`;
+
+export const Link = styled.a`
+  color: #315efb;
+  text-decoration: underline;
+`;
